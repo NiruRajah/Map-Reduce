bb115aa408c4216f56a747a7ff1bfd280191305a
Nirusan <shanrajah@hotmail.com>
2020-12-06 02:17:08 -0500

    Added reduce for number of samples

--- reduceForNumberOfSamples.js
+++ reduceForNumberOfSamples.js
@@ -1,7 +1,13 @@
-function reduceForNumberOfSamples(key, values){
-    var total  = 0;
-    for(var i = 0; i < values.length; i++){
-        total += values[i].count;
+function reduceForNumberOfSamples(keyRange, valuesObject)
+{
+    var reducedVal = { count:0, MemoryUtilization_Average:0}
+    for(var i = 0; i < valuesObject.length; i++){
+        reducedVal.count += valuesObject[i].count;
+        reducedVal.MemoryUtilization_Average += (valuesObject[i].MemoryUtilization_Average);
+        //reducedVal.MemoryUtilization_Average = valuesObject[i].MemoryUtilization_Average;
     }
-    return{count: total};
+    
+
+    return reducedVal;
+
 }
