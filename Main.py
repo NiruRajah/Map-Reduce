ec068130e10f9356fa750d1bd9def5406e69d321
Nirusan <shanrajah@hotmail.com>
2020-12-06 02:15:39 -0500

    Added map logic for CPU

--- Main.py
+++ Main.py
@@ -6,26 +6,40 @@ import os
 import csv
 from datetime import datetime
 
-#client = pymongo.MongoClient("mongodb+srv://Nirusan:CloudProject@mapreduce.k9wk8.mongodb.net/test?retryWrites=true&w=majority")
 client = pymongo.MongoClient("mongodb+srv://Nirusan:CloudProject@mapreduce.k9wk8.mongodb.net/test?retryWrites=true&w=majority")
 db = client["Workload"]
 col1 = db["DVD-testing"]
 col2 = db["DVD-training"]
 col = db["Dell-DVD-Datasets"]
 
-mapf = Code(open('mapOfCPUUtilization_Average.js', 'r').read())
+mapOfCPU = Code(open('mapOfCPU.js', 'r').read())
 
-reduceForAverage = Code(open('reduceForAverage.js', 'r').read())
+#mapc = Code(open('mapOfMemoryUtilization_Average.js', 'r').read())
+
+finalizeForAverage = Code(open('finalizeForAverage.js', 'r').read())
+
+reduceForMedian = Code(open('reduceForMedian.js', 'r').read())
+
+finalizeForMedian = Code(open('finalizeForMedian.js', 'r').read())
+
+
 
 reduceForNumberOfSamples = Code(open('reduceForNumberOfSamples.js', 'r').read())
 
+reduceValues = Code(open('reduce.js', 'r').read())
+
+finalizeValue = Code(open('finalize.js', 'r').read())
 
 
 
-result = col.map_reduce(mapf, reduceForAverage, 'myresults')
+#result = col.map_reduce(mapf, reduceForNumberOfSamples, 'myresults')
+#for i in result.find():
+#    print(i)
+
+#result2 = col.map_reduce(mapf, reduceForNumberOfSamples, 'myresults2', finalize=finalizeForAverage)
+#for i in result2.find():
+#    print(i)
+
+result = col.map_reduce(mapOfCPU, reduceValues, 'result', finalize=finalizeValue)
 for i in result.find():
-    print(i)
-
-result2 = col.map_reduce(mapf, reduceForNumberOfSamples, 'myresults2')
-for i in result2.find():
     print(i)
---
+++ mapOfCPU.js
@@ -0,0 +1,57 @@
+function mapOfCPU()
+{
+    
+    var key = "";
+    
+    if(parseInt(this.CPUUtilization_Average) >= 0 && parseInt(this.CPUUtilization_Average) <= 10)
+    {
+        key = "Range: [1, 10]";
+    }
+    else if(parseInt(this.CPUUtilization_Average) > 10 && parseInt(this.CPUUtilization_Average) <= 20)
+    {
+        key = "Range: [10, 20]";
+    }
+    else if(parseInt(this.CPUUtilization_Average) > 20 && parseInt(this.CPUUtilization_Average) <= 30)
+    {
+        key = "Range: [20, 30]";
+    }
+    else if(parseInt(this.CPUUtilization_Average) > 30 && parseInt(this.CPUUtilization_Average) <= 40)
+    {
+        key = "Range: [30, 40]";
+    }
+    else if(parseInt(this.CPUUtilization_Average) > 40 && parseInt(this.CPUUtilization_Average) <= 50)
+    {
+        key = "Range: [40, 50]";
+    }
+    else if(parseInt(this.CPUUtilization_Average) > 50 && parseInt(this.CPUUtilization_Average) <= 60)
+    {
+        key = "Range: [50, 60]";
+    }
+    else if(parseInt(this.CPUUtilization_Average) > 60 && parseInt(this.CPUUtilization_Average) <= 70)
+    {
+        key = "Range: [60, 70]";
+    }
+    else if(parseInt(this.CPUUtilization_Average) > 70 && parseInt(this.CPUUtilization_Average) <= 80)
+    {
+        key = "Range: [70, 80]";
+    }
+    else if(parseInt(this.CPUUtilization_Average) > 80 && parseInt(this.CPUUtilization_Average) <= 90)
+    {
+        key = "Range: [80, 90]";
+    }
+    else if(parseInt(this.CPUUtilization_Average) > 90 && parseInt(this.CPUUtilization_Average) <= 100)
+    {
+        key = "Range: [90, 100]";
+    }
+
+    var value = {
+                    count: 1, 
+                    sum: parseFloat(this.MemoryUtilization_Average),
+                    min: parseFloat(this.MemoryUtilization_Average),
+                    max: parseFloat(this.MemoryUtilization_Average),
+                    sumOfNumberMinusMeanSquared: 0
+                };
+
+    emit(key, value);
+    
+}
